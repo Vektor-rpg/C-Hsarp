227cc4f3ea658b8a0047d90c39c24dd13da352c5
Artem <Vektor-rpg@yandex.ru>
2020-04-09 00:11:10 +0700

    Задание 1.

---
+++ home task 4/home task 4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace home_task_4
+{
+    class Program
+    {
+        //Дан целочисленный массив из 20 элементов.Элементы массива могут принимать целые значения от –10 000 до 10 000 включительно.
+        //Написать программу, позволяющую найти и вывести количество пар элементов массива, в которых хотя бы одно число делится на 3.
+        static int GetNumber(int[] array, int length)
+        {
+            int amountOfPairs = 0;
+            for (int i = 0; i < length - 1; i++)
+            {
+                if (array[i] % 3 == 0 || array[i + 1] % 3 == 0)
+                {
+                    amountOfPairs++;
+                }
+            }
+            return amountOfPairs;
+        }
+
+        static void Main(string[] args)
+        {
+            int arrayLength = 20;
+            int[] myArray = new int[arrayLength];
+            Random random = new Random();
+            int result;
+
+            Console.WriteLine("Вас приветствует программа подсчёта пар элементов, в которых хотя бы одно число делится на 3.");
+            Console.Write("\nВ следующем массиве [ ");
+            for (int i = 0; i < arrayLength; i++)
+            {
+                myArray[i] = random.Next(-10000, 10000);
+                Console.Write(myArray[i] + ", ");
+            }
+            Console.WriteLine("\b\b ]\n");
+
+            result = GetNumber(myArray, arrayLength);
+
+            Console.WriteLine($"Количество пар: {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ home task 4/home task 4/View.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace home_task_4
+{
+    public class View
+    {
+        public static void Print(object value, bool isNewLine = true)
+        {
+            if (isNewLine)
+            {
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.Write(value);
+            }
+
+        }
+
+        public static int GetInt()
+        {
+            int result = 0;
+            do
+            {
+                Print("Введите число");
+            } while (!Int32.TryParse(GetString(), out result));
+            return result;
+        }
+
+        public static double GetDouble()
+        {
+            double result = 0.0d;
+            do
+            {
+                Print("Введите число");
+            } while (!Double.TryParse(GetString().Replace(".", ","), out result));
+            return result;
+        }
+
+        public static string GetString()
+        {
+            return Console.ReadLine();
+        }
+
+        public static void Pause()
+        {
+            Console.ReadKey();
+        }
+
+        public static void PrintArray(int[] arr, char s = '-')
+        {
+            for (int index = 0; index < arr.Length; index++)
+            {
+                var i = arr[index];
+                Print($"{s}{i}");
+            }
+        }
+
+        public static void PrintMatrix(int[,] array)
+        {
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    Print($" {array[i, j]}", false);
+                }
+
+                Print(" ");
+            }
+        }
+    }
+}
